0b22bfbbeaee42348c58abdf246a6e753845c19a
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-01-27 22:40:37 +0100

    javaScript done and dusted

---
+++ script.js
@@ -0,0 +1,32 @@
+const form = document.querySelector('form');
+const firstName = document.querySelector('#firstName');
+const lastName = document.querySelector('#lastName');
+const email = document.querySelector('#email');
+const dob = document.querySelector('#dob');
+const password = document.querySelector('#password');
+const confirmPassword = document.querySelector('#confirmPassword');
+const msg = document.querySelector('.msg');
+
+form.addEventListener('submit', submitForm);
+
+function submitForm(e) {
+    e.preventDefault();
+
+    //  lastName.value === '' email.value === '' dob.value === ''
+    if (firstName.value === '') {
+        if (password.value === !confirmPassword.value) {
+            msg.innerHTML = 'incorrect Password';
+        }
+        else {
+            msg.innerHTML = 'please enter all field';
+        }
+
+        setTimeout(() => msg.remove(), 3000);
+    } else {
+        console.log('form submitted successfully')
+        window.alert('Form Submitted Successfully')
+        firstName.value && lastName.value && email.value && dob.value === '';
+    }
+
+
+}
