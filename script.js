940461d69b81b8d4590f7d87a661cbf180b39698
Sallah10 <107303495+Sallah10@users.noreply.github.com>
2025-01-29 23:44:28 +0100

    code cleanup

--- script.js
+++ script.js
@@ -1,37 +1,3 @@
-// const form = document.querySelector('form');
-// const firstName = document.querySelector('#firstName');
-// const lastName = document.querySelector('#lastName');
-// const email = document.querySelector('#email');
-// const dob = document.querySelector('#dob');
-// const password = document.querySelector('#password');
-// const confirmPassword = document.querySelector('#confirmPassword');
-// const msg = document.querySelector('.msg');
-
-// form.addEventListener('submit', submitForm);
-
-// function submitForm(e) {
-//     e.preventDefault();
-
-//     //  lastName.value === '' email.value === '' dob.value === ''
-//     if (firstName.value === '' || lastName.value === '' || email.value === '' || dob.value === '') {
-//         if (password.value === confirmPassword.value) {
-//             msg.innerHTML = 'incorrect Password';
-//             window.alert('incorrect Password')
-//         }
-//         else {
-//             msg.innerHTML = 'please enter all field';
-//             window.alert('please enter all field')
-//         }
-
-//         setTimeout(() => msg.remove(), 3000);
-//     } else {
-//         console.log('form submitted successfully')
-//         window.alert('Form Submitted Successfully')
-//         firstName.value && lastName.value && email.value && dob.value === '';
-//     }
-
-
-// }
 const form = document.querySelector('form');
 const firstName = document.querySelector('#firstName');
 const lastName = document.querySelector('#lastName');
@@ -40,7 +6,6 @@ const dob = document.querySelector('#dob');
 const password = document.querySelector('#password');
 const confirmPassword = document.querySelector('#confirmPassword');
 const termsCheckbox = document.querySelector('#terms');
-// const RememberMe = document.querySelector('#rememberMe');
 const msg = document.querySelector('.msg');
 
 form.addEventListener('submit', submitForm);
@@ -70,13 +35,8 @@ function submitForm(e) {
         msg.textContent = 'You must agree to the terms and conditions';
         msg.style.color = 'red';
         window.alert('You must agree to the terms and conditions');
-        // } else if (!RememberMe.checked) {
-        //     // Check if the checkbox is checked
-        //     msg.textContent = 'You must agree to the terms and conditions';
-        //     msg.style.color = 'red';
-        //     window.alert('You must agree to the terms and conditions');
-        // } else {
-        // Success: Form submission
+    } else {
+        //Success statement: Form submission
         msg.textContent = 'Form submitted successfully!';
         msg.style.color = 'green';
         window.alert('Form submitted successfully!');
